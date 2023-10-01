b9b4f81d80f4fbfd7c813a820b1370456745a308
tm-laurenzo <60081627+tm-laurenzo@users.noreply.github.com>
2023-10-01 20:35:10 +0100

    removed unnecessary comments

--- SignalRImplementation/Program.cs
+++ SignalRImplementation/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-//builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddSignalR();
 builder.Services.AddCors(options =>
@@ -17,24 +17,17 @@ builder.Services.AddCors(options =>
             .AllowCredentials();
     });
 });
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
+    
     app.UseDeveloperExceptionPage();
 }
 
-//app.UseHttpsRedirection();
 
-//app.UseAuthorization();
-
-//app.MapControllers();
 app.UseRouting();
 app.UseCors();
 app.UseEndpoints(endpoints =>
